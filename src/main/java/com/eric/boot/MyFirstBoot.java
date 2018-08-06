69b75f74323e6ba6c8728d508d9f3cf5f6e98c58
EricHo0129 <yung.ho@104.com.tw>
2018-08-06 10:40:40 +0800

    加上參數讀取

--- src/main/java/com/eric/boot/MyFirstBoot.java
+++ src/main/java/com/eric/boot/MyFirstBoot.java
@@ -1,17 +1,24 @@
 package com.eric.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eric.boot.config.ConfigBean;
+
 @SpringBootApplication
 @RestController
 public class MyFirstBoot {
 
+	@Autowired
+	private ConfigBean configBean;
+	
 	@RequestMapping("/")
 	public String index() {
-		return "Hello world!";
+		return "Server["+configBean.getServer()+":"+configBean.getPort()+"] said: Hello world!";
 	}
 	
 	public static void main(String[] args) {
---
+++ src/main/java/com/eric/boot/config/ConfigBean.java
@@ -0,0 +1,29 @@
+package com.eric.boot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix="com.eric.boot")
+@PropertySource("classpath:application.properties")
+public class ConfigBean {
+
+	private String server;
+	private int port;
+	
+	public String getServer() {
+		return server;
+	}
+	public void setServer(String server) {
+		this.server = server;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+}
